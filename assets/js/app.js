5ca4fdf8257155469b565c84d935b20696db9f76
devohme <roby@ohme.kr>
2025-11-05 08:24:48 +0700

    manipulasi waktu

--- assets/js/app.js
+++ assets/js/app.js
@@ -60,11 +60,20 @@ function loadStatus() {
             const dataMap = new Map(data.map(item => [item.line_id, item]));
             let hasDisconnected = false;
 
+            const now = new Date();
+            const year = now.getFullYear();
+            const month = String(now.getMonth() + 1).padStart(2, '0');
+            const day = String(now.getDate()).padStart(2, '0');
+            const hours = String(now.getHours()).padStart(2, '0');
+            const minutes = String(now.getMinutes()).padStart(2, '0');
+            const seconds = String(now.getSeconds()).padStart(2, '0');
+            const formatted = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
             fixedLocations.forEach(loc => {
                 const item = dataMap.get(loc);
                 // let status = "UNKNOWN";
                 let status = "OK";
-                let timestamp = "-";
+                let timestamp = formatted;
 
                 // Logika kini sederhana: hanya baca status dari server
                 if (item) {
