0ca9303c85e1a86eef8ed58c9cc2385523cacab3
devohme-id <roby@ohme.kr>
2025-12-09 22:04:43 +0700

    fix style

--- temperature_monitor.php
+++ temperature_monitor.php
@@ -13,28 +13,38 @@
 </head>
 
 <body>
-
     <header class="header-ui">
-        <div class="header-center">
-            <h1 class="header-title" id="header-title-text">
-                ENVIRONMENT MONITORING
-            </h1>
+        <div class="header-left">
+            <h1 class="header-title" id="header-title-text">ENVIRONMENT MONITORING</h1>
             <p class="header-subtitle" id="header-subtitle-text"></p>
         </div>
         <div class="header-right">
-            <div class="dropdown-container">
-                <button class="nav-btn" onclick="toggleDropdown()" title="Menu">
-                    <i class="ti ti-menu-2" style="font-size: 1.2em;"></i>
-                    <span>MENU</span>
-                </button>
-                <div id="nav-dropdown" class="dropdown-menu">
-                    <a href="#" class="dropdown-item" onclick="handleNavClick(1); return false;" id="link-page-1">SMT Lines</a>
-                    <a href="#" class="dropdown-item" onclick="handleNavClick(2); return false;" id="link-page-2">Area & Storage</a>
-                    <a href="#" class="dropdown-item" onclick="handleNavClick(3); return false;" id="link-page-3">Facility</a>
-                    <a href="grounding_monitor.php" class="dropdown-item" target="_top">Grounding Monitor</a>
+            <nav class="nav-menu">
+                <div class="dropdown-container">
+                    <button class="dropdown-toggle" onclick="toggleDropdown()" title="Menu">
+                        <i class="ti ti-menu-2" style="font-size: 1.2em;"></i>
+                        <span>MENU</span>
+                    </button>
+                    <div id="nav-dropdown" class="dropdown-menu">
+                        <a href="#" class="dropdown-item" onclick="handleNavClick(1); return false;" id="link-page-1">SMT Lines</a>
+                        <a href="#" class="dropdown-item" onclick="handleNavClick(2); return false;" id="link-page-2">Area & Storage</a>
+                        <a href="#" class="dropdown-item" onclick="handleNavClick(3); return false;" id="link-page-3">Facility</a>
+                        <a href="grounding_monitor.php" class="dropdown-item" target="_top">Grounding Monitor</a>
+                    </div>
                 </div>
-            </div>
-
+            </nav>
+            <button id="audio-toggle" class="sound-btn" title="Enable/Disable Sound">
+                <div id="audio-icon-container" class="flex items-center justify-center">
+                    <svg class="sound-icon muted-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M17 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2" />
+                    </svg>
+                    <svg class="sound-icon unmuted-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728" />
+                    </svg>
+                </div>
+            </button>
             <div class="header-clock">
                 <div id="time-display" class="clock-time"></div>
                 <div id="date-display" class="clock-date"></div>
